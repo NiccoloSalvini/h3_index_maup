0a6164556a7ff4aa5f247392281e5bcc7f5b0644
Niccolò Salvini <niccolo.salvini27@gmail.com>
2023-05-19 19:33:38 +0100

    develop notebook 01 nd 02

---
+++ notebooks/01_test_how_h3_works.R
@@ -0,0 +1,48 @@
+## remotes::install_github("crazycapivara/h3-r")
+library(h3)
+
+# Rendering
+library(leaflet)
+
+# Binning
+# takes lat long and map back to unique hexagon
+h3_index <- geo_to_h3(road_safety_greater_manchester)
+
+## table how many accidents per hex
+tbl <- table(h3_index) %>%
+  tibble::as_tibble()
+
+## convert to `sf` and rename columns
+hexagons <- h3_to_geo_boundary_sf(tbl$h3_index) %>%
+  dplyr::mutate(index = tbl$h3_index, accidents = tbl$n)
+
+pal <- colorBin("YlOrRd", domain = hexagons$accidents)
+
+map <- leaflet(data = hexagons, width = "100%") %>%
+  addProviderTiles("Stamen.Toner") %>%
+  addPolygons(
+    weight = 2,
+    color = "white",
+    fillColor = ~ pal(accidents),
+    fillOpacity = 0.8,
+    label = ~ sprintf("%i accidents (%s)", accidents, index)
+  )
+
+map
+
+## sono organizzati come un grafo ( solo 1 parente ma più figli)
+## come funzionano i parenti via h3_to_parent
+h3_index_res_10<- geo_to_h3(road_safety_greater_manchester, res = 10)
+
+# nessun parente con sè stesso
+h3_to_parent(h3_index_res_4, res = 10)
+
+# parenteì: 8419519ffffffff
+h3_to_parent(h3_index_res_4, res = 4)
+
+## figli
+h3_to_children(h3_index_res_4, res = 11)
+
+
+
+s
--- notebooks/02_analysis.R
+++ notebooks/02_analysis.R
@@ -1,11 +1,59 @@
 
-library(h3)
 
-simulated_data =data.frame(lat = sample(-90:90, 1000, replace = TRUE), lng = sample(-180:180, 1000, replace = TRUE))
+# Example of fitting a spatial autoregressive model
+model <- spdep::lagsarlm(
+  formula = value ~ 1,
+  data = hexagons,
+  listw = hex_to_hex_2d(list(hexagons$h3_id)),
+  method = "BM",
+  tol.solve = 1e-6,
+  zero.policy = TRUE
+)
+
+summary(model)
+
+# Example of incorporating uncertainty analysis
+# we may think of having different level of spatial autocorelation
+simulations <- replicate(10, hexagons %>% mutate(value = rnorm(n(), value, sd = 0.1 * value)))
+
+
+aggregate_and_summarise <- function(.data, col_grouping, col_value) {
+  .data %>%
+    group_by(col_grouping) %>%
+    summarise(mean_value = mean(col_value), sd_value = sd(col_value)) %>%
+    return()
+}
+
+
+simulation_summary <- simulations %>%
+  map_dfr(~aggregate_and_summarise(.x, col_grouping = h3_id, col_value = value))
+
+group_by(h3_id) %>%
+  summarise(
+    mean_value = mean(mean_value),
+    sd_value = mean(sd_value)
+  ) %>%
+  mutate(coeff_var = sd_value / mean_value)
+
+
+simulation_summary <- simulations %>%
+  map_dfr(~ .x %>%
+            group_by(h3_id) %>%
+            summarise(mean_value = mean(value), sd_value = sd(value)))
+  group_by(h3_id) %>%
+  summarise(
+    mean_value = mean(mean_value),
+    sd_value = mean(sd_value)
+  ) %>%
+  mutate(coeff_var = sd_value / mean_value)
 
 
 
-hexagons <- geo_to_h3(simulated_data, res = 1)
+
+
+
+
+
 
 
 
