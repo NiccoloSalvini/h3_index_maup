14d8c38acde76f30f504fe2a0efa5d807cf63b11
Vincenzo Nardelli <vincnardelli@gmail.com>
2024-01-26 09:52:40 +0100

    Update simulation

--- notebooks/06_simulation.R
+++ notebooks/06_simulation.R
@@ -7,8 +7,9 @@ library(parallel)
 library(igraph)
 library(patchwork)
 library(scales)
+library(Matrix)
 
-map <- read_sf("data/ProvCM01012023_g/ProvCM01012023_g_WGS84.shp")
+
 mstconnect <- function(polys, nb, distance="centroid"){
 
   if(distance == "centroid"){
@@ -30,6 +31,7 @@ mstconnect <- function(polys, nb, distance="centroid"){
   allnb
 }
 
+map <- read_sf("data/ProvCM01012023_g/ProvCM01012023_g_WGS84.shp")
 gen_hmap <- function(map, res=1){
   map %>%
     st_union() %>%
@@ -38,105 +40,132 @@ gen_hmap <- function(map, res=1){
     h3_to_geo_boundary_sf()
 }
 
-gen_points <- function(map, size=500, type="random", rho=0){
-  points <- st_sample(map, size = size, type = type, exact = TRUE) %>%
-    st_as_sf()
+gen_points <- function(base_map, rho=0){
+  nb <-poly2nb(base_map, queen=T)
+  nb <- mstconnect(base_map, nb)
 
-  neighbors <- knn2nb(knearneigh(st_coordinates(points), k = 5))
-  W <- nb2listw(neighbors, style="W")
-  W_mat<-nb2mat(neighbors)
-  I<-diag(1,nrow(points), nrow(points))
-  y <- rnorm(nrow(points), mean=5, sd=1)
-  y <- solve(I-rho*W_mat)%*%(y)
-  points$y <- y
-  return(list(points = points, W=W))
+  W_mat<-nb2mat(nb)
+  W <- nb2listw(nb)
+  #I<-diag(1,nrow(base_map), nrow(base_map))
+  y <- rnorm(nrow(base_map), mean=0, sd=1)
+#  y <- solve(I-rho*W_mat)%*%(y)
+  #y <- solve(I - rho * W_mat, y)
+
+  W_mat_sparse <- as(W_mat, "sparseMatrix")
+
+  # The identity matrix should also be a sparse matrix for efficient computation
+  I_sparse <- Diagonal(x = rep(1, nrow(W_mat_sparse)))
+
+  # Solve the linear system (I - rho * W_mat) * x = y using sparse matrix operations
+  # The solve function from the Matrix package efficiently handles sparse matrices
+  y <- solve(I_sparse - rho * W_mat_sparse, y)
+
+  y <- as.numeric((y-mean(y))/sd(y))
+  return(list(y = y, W=W))
 }
-
 simulate_res_impact <- function(res, points){
   hmap <- base_hmaps[[res]]
-  values <- st_join(st_as_sf(points) %>%
-                      st_transform(4326), hmap, join = st_within) %>%
+
+  values <- base_map %>%
+    mutate(y= points$y) %>%
+    st_centroid() %>%
+    select(-h3_index) %>%
+    st_join(hmap, join = st_within) %>%
+    st_drop_geometry() %>%
     group_by(h3_index) %>%
     summarise(y = mean(y, na.rm = TRUE)) %>%
     st_drop_geometry() %>%
     filter(!is.na(h3_index))
+
   hmap <- hmap %>%
     left_join(values, by = join_by(h3_index)) %>%
     filter(!is.na(y))
 
   nb <-poly2nb(hmap, queen=T)
-  nb <- mstconnect(hmap, nb, distance = "centroid")
-  #plot(nb, st_coordinates(st_centroid(hmap)))
+  nb <- mstconnect(hmap, nb)
   W_hmap <- nb2listw(nb)
-  moran <- moran.mc(values$y, W_hmap, 999)
-  return(data.frame(moran=moran$statistic, p=moran$p.value))
+  moran <- moran.mc(hmap$y, W_hmap, 999)
+  return(data.frame(mean = mean(hmap$y), sd=sd(hmap$y), moran=moran$statistic, p=moran$p.value))
 }
+
+
+
 
 
 # Simulation ----
-res_vector <- 3:6
-rhos <- 0
+res_vector <- 3:5
 rhos <- c(0, 0.2, 0.4, 0.6, 0.8)
-types <- c("random", "regular")
-num_sim <- 500
+num_sim <- 100
 base_hmaps <- lapply(res_vector, gen_hmap, map=map)
+base_map <- base_hmaps[[3]]
 
 
-simulation <- function(sim_id, rho, type){
-  system(sprintf('echo "\n%s\n"', paste(sim_id, "-", rho, "-", type)))
-
-  points_generated <- gen_points(map, rho=rho, type=type)
-  points <- points_generated$points
-  W <- points_generated$W
-  moran <- moran.mc(points$y, W, 999)
-  real <- data.frame("moran"=moran$statistic, "p"=moran$p.value)
-  colnames(real) <- c("moran", "p")
+simulation <- function(sim_id, rho){
+  system(sprintf('echo "\n%s\n"', paste(sim_id, "-", rho)))
+  points <- gen_points(base_map, rho)
   results <- lapply(1:length(res_vector), simulate_res_impact, points=points)
   results_df <- do.call(rbind, results)
-  final <- rbind(real, results_df)
-  rownames(final) <- NULL
-  final$res <- c(0, res_vector)
-  final$sim_id = sim_id
-  final$rho = rho
-  final$type = type
-  return(final)
+  rownames(results_df) <- NULL
+  results_df$res <- res_vector
+  results_df$sim_id = sim_id
+  results_df$rho = rho
+  return(results_df)
 }
 
-experiments <- expand.grid(sim_id=1:num_sim,
-                           rho=rhos,
-                           type= types)
-simulation_results <- mclapply(1:nrow(experiments), function(id) simulation(sim_id=experiments$sim_id[id],
-                                                                            rho = experiments$rho[id],
-                                                                            type = experiments$type[id]),
-                               mc.cores = 11, mc.preschedule = FALSE)
+# simulation(1, 0.8)
+#
+# experiments <- expand.grid(sim_id=1:num_sim,
+#                            rho=rhos)
+# simulation_results <- lapply(1:nrow(experiments), function(id) simulation(sim_id=experiments$sim_id[id],
+#                                                                             rho = experiments$rho[id]))
+# results <- do.call(rbind, simulation_results)
+# results
+
+numCores <- detectCores()
+cl <- makeCluster(numCores-1)
+experiments <- expand.grid(sim_id=1:num_sim, rho=rhos)
+clusterExport(cl, varlist = c("simulation", "experiments", "gen_points",
+                              'base_map',"mstconnect",
+                              "Diagonal", 'simulate_res_impact',
+                              'res_vector','base_hmaps'))
+
+clusterEvalQ(cl, {
+  library(dplyr)
+  library(spdep)
+  library(sf)
+})
+
+simulation_results <- parLapply(cl, 1:nrow(experiments), function(id) {
+  simulation(sim_id=experiments$sim_id[id], rho=experiments$rho[id])
+})
+
+stopCluster(cl)
+
 results <- do.call(rbind, simulation_results)
+results
+
 
 
 # Graphs ----
 
 # Fig 1
-
-map_example_reg <- gen_points(map, size=500, type="regular")$points %>%
-  st_as_sf(points) %>%
-  st_transform(4326)
-
-f1_1 <- ggplot() +
-  geom_sf(data=map) +
-  geom_sf(data=map_example_reg, size=0.8) +
+example_map <- gen_points(base_map, rho=0)
+m1 <- base_map %>%
+  mutate(y=example_map$y) %>%
+  ggplot() +
+  geom_sf(aes(fill=y)) +
   theme_minimal() +
-  ggtitle("Regular pattern")
+  ggtitle("No autocorrelation")
 
-map_example_ran <- gen_points(map, size=500, type="random")$points %>%
-  st_as_sf(points) %>%
-  st_transform(4326)
-
-f1_2 <- ggplot() +
-  geom_sf(data=map) +
-  geom_sf(data=map_example_ran, size=0.8) +
+example_map <- gen_points(base_map, rho=0.8)
+m2 <- base_map %>%
+  mutate(y=example_map$y) %>%
+  ggplot() +
+  geom_sf(aes(fill=y)) +
   theme_minimal() +
-  ggtitle("Random pattern")
+  ggtitle("Positive autocorrelation")
 
-f1 <- f1_1 + f1_2
+f1 <- m1  + m2
 ggsave("export/f1.pdf", f1, width = 1200, height=600, units = "px", scale=2)
 
 
@@ -156,24 +185,20 @@ f2_3 <- ggplot() +
   theme_minimal() +
   ggtitle("H3 Res 5")
 
-f2_4 <- ggplot() +
-  geom_sf(data=base_hmaps[[4]]) +
-  theme_minimal() +
-  ggtitle("H3 Res 6")
 
 
-f2 <- (f2_1 + f2_2 + f2_3 + f2_4) +
-  plot_layout(ncol = 4)
+f2 <- (f2_1 + f2_2 + f2_3) +
+  plot_layout(ncol = 3)
 f2
 ggsave("export/f2.pdf", f2, width = 1200, height=400, units = "px", scale=2)
 
 
 # Fig 3
 f3 <- results %>%
-  filter(res == 0) %>%
+  filter(res == 5) %>%
   ggplot() +
   geom_density(aes(moran, color=factor(rho))) +
-  facet_wrap(~type, nrow=1) +
+  #facet_wrap(~type, nrow=1) +
   theme_bw() +
   xlab("Moran's I") +
   labs(color=expression(rho))
@@ -184,7 +209,7 @@ f4 <- results %>%
   filter(res != 0) %>%
   ggplot() +
   geom_density(aes(moran, color=factor(rho))) +
-  facet_wrap(type~res, nrow=2) +
+  facet_wrap(res~., ncol=3) +
   theme_minimal() +
   theme_bw() +
   xlab("Moran's I") +
@@ -192,99 +217,61 @@ f4 <- results %>%
 ggsave("export/f4.pdf", f4, width = 1200, height=600, units = "px", scale=2)
 
 
-
-results %>%
-  filter(res != 0) %>%
-  left_join(results_target, by=c("sim_id", "rho", "type")) %>%
-  mutate(test = p < 0.05 & p_target < 0.05 |p > 0.05 & p_target > 0.05) %>%
-  group_by(res, rho, type) %>%
-  summarise(test = mean(test)) %>%
-  ggplot() +
-  geom_col(aes(rho, test, fill=factor(res)), position='dodge') +
-  facet_wrap(~type, nrow=1)
-
-# Fig 5
 f5 <- results %>%
-  filter(res != 0) %>%
-  left_join(results_target, by=c("sim_id", "rho", "type")) %>%
-  mutate(test = p < 0.05 & p_target < 0.05 | p > 0.05 & p_target > 0.05) %>%
-  group_by(res, rho, type) %>%
-  summarise(test = mean(test)) %>%
+  group_by(res, rho) %>%
+  summarise(mean = mean(mean)) %>%
+  mutate(rho=as.factor(rho)) %>%
   ggplot() +
-  geom_col(aes(rho, test, fill=factor(res)), position='dodge') +
-  facet_wrap(~type, nrow=1) +
-  scale_y_continuous(labels = percent) +  # Format y-axis as percentage
-  scale_x_continuous(breaks = c(0, 0.2, 0.4, 0.6, 0.8)) +  # Set x-axis breaks
-  ylab("Correct Tests") +  # Set y-axis label
-  xlab(expression(rho)) +  # Set x-axis label
-  labs(fill="Resolution") + # Set legend title
-  theme_bw()
-
-ggsave("export/f5.pdf", f5, width = 1200, height=400, units = "px", scale=2)
+  geom_line(aes(res, mean, color=rho)) +
+  theme_minimal() +
+  theme_bw() +
+  xlab("H3 Res") +
+  labs(color=expression(rho)) +
+  ylim(c(-1, 1)) +
+  scale_x_continuous(breaks=res_vector)
+ggsave("export/f5.pdf", f5, width = 1200, height=600, units = "px", scale=2)
 
 
-
-# Other graphs ----
-
-results_target <- results %>%
-  filter(res == 0) %>%
-  select(sim_id, rho, type, moran_target = moran, p_target = p)
-
-results %>%
-  filter(res != 0) %>%
-  left_join(results_target, by=c("sim_id", "rho", "type")) %>%
-  mutate(bias = abs(moran - moran_target)) %>%
-  group_by(rho) %>%
-  summarise(bias = mean(bias))
-
-
-results %>%
-  filter(res != 0) %>%
-  left_join(results_target, by=c("sim_id", "rho", "type")) %>%
-  mutate(bias = (moran - moran_target)^2) %>%
-  group_by(res, rho, type) %>%
-  summarise(bias = mean(bias)) %>%
+f6 <- results %>%
+  group_by(res, rho) %>%
+  summarise(sd = mean(sd)) %>%
+  mutate(rho=as.factor(rho)) %>%
   ggplot() +
-  geom_line(aes(res, bias, color=factor(rho))) +
-  geom_point(aes(res, bias, color=factor(rho))) +
-  facet_wrap(~type, nrow=2)
+  geom_line(aes(res, sd, color=rho)) +
+  theme_minimal() +
+  theme_bw() +
+  xlab("H3 Res") +
+  labs(color=expression(rho)) +
+  scale_x_continuous(breaks=res_vector)
+ggsave("export/f6.pdf", f6, width = 1200, height=600, units = "px", scale=2)
 
-results %>%
-  filter(res != 0) %>%
-  left_join(results_target, by=c("sim_id", "rho", "type")) %>%
-  mutate(bias = abs((moran - moran_target)/moran_target)) %>%
-  group_by(res, rho, type) %>%
-  summarise(MSE = mean(bias)) %>%
+f7 <- results %>%
+  group_by(res, rho) %>%
+  summarise(moran = mean(moran)) %>%
+  mutate(rho=as.factor(rho)) %>%
   ggplot() +
-  geom_line(aes(rho, MSE, color=factor(res))) +
-  geom_point(aes(rho, MSE, color=factor(res))) +
-  facet_wrap(~type, nrow=2)
+  geom_line(aes(res, moran, color=rho)) +
+  theme_minimal() +
+  theme_bw() +
+  xlab("H3 Res") +
+  ylab("Moran's I") +
+  labs(color=expression(rho)) +
+  scale_x_continuous(breaks=res_vector)
+ggsave("export/f7.pdf", f7, width = 1200, height=600, units = "px", scale=2)
 
-
-results %>%
-  filter(res != 0) %>%
-  left_join(results_target, by=c("sim_id", "rho", "type")) %>%
-  mutate(test = p < 0.05 & p_target < 0.05 |p > 0.05 & p_target > 0.05) %>%
-  group_by(res, rho, type) %>%
-  summarise(test = mean(test))
-
-results %>%
-  filter(res != 0) %>%
-  left_join(results_target, by=c("sim_id", "rho", "type")) %>%
-  mutate(test = p < 0.05 & p_target < 0.05 |p > 0.05 & p_target > 0.05) %>%
-  group_by(res, rho, type) %>%
-  summarise(test = mean(test)) %>%
+f8 <- results %>%
+  group_by(res, rho) %>%
+  mutate(p_value = p < 0.05) %>%
+  summarise(p = mean(p_value)) %>%
+  mutate(rho=as.factor(rho)) %>%
   ggplot() +
-  geom_line(aes(rho, test, color=factor(res))) +
-  geom_point(aes(rho, test, color=factor(res))) +
-  facet_wrap(~type, nrow=2)
-
-results %>%
-  filter(res != 0) %>%
-  left_join(results_target, by=c("sim_id", "rho", "type")) %>%
-  mutate(test = p < 0.05 & p_target < 0.05 |p > 0.05 & p_target > 0.05) %>%
-  group_by(res, rho, type) %>%
-  summarise(test = mean(test)) %>%
-  ggplot() +
-  geom_col(aes(rho, test, fill=factor(res)), position='dodge') +
-  facet_wrap(~type, nrow=2)
+  geom_line(aes(res, p, color=rho)) +
+  geom_point(aes(res, p, color=rho)) +
+  theme_minimal() +
+  geom_hline(yintercept=0.05) +
+  theme_bw() +
+  xlab("H3 Res") +
+  ylab("Power") +
+  labs(color=expression(rho)) +
+  scale_x_continuous(breaks=res_vector)
+ggsave("export/f8.pdf", f8, width = 1200, height=600, units = "px", scale=2)
